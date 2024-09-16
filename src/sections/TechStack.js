2d474769a295ace2ee5b5249e2e33d7b8841947b
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-09-16 09:32:02 +0300

    updata contact section and techstach section

--- src/sections/TechStack.js
+++ src/sections/TechStack.js
@@ -8,21 +8,23 @@ function TechStack() {
         Tech <span className="text-wood">Stack</span>
       </h1>
       <div className="grid grid-cols-2 grid-flow-row gap-4 sm:grid-cols-1 nd:grid-cols-3  gap-y-20 justify-items-center pb-20">
-        <div class="relative shadow-lg rounded-full  h-40 w-40 sm:h-auto sm:w-64 flex items-center grid justify-items-center  ">
+        <div class="relative shadow-lg rounded-full  h-40 w-40 sm:h-40 sm:w-40 flex items-center grid justify-items-center  ">
           <div>
             <img src={python} />
           </div>
         </div>
-        <div class="relative shadow-lg rounded-full  h-40 w-40 sm:h-auto sm:w-64 flex items-center grid justify-items-center  ">
-          <div>
-            <img src={rust} />
-          </div>
-        </div>
-        <div class="relative shadow-lg rounded-full  h-40 w-40 sm:h-auto sm:w-64 flex items-center grid justify-items-center  ">
+        <div class="relative shadow-lg rounded-full  h-40 w-40 sm:h-40 sm:w-40 flex items-center grid justify-items-center  ">
           <div>
             <img src={js} />
           </div>
         </div>
+        <div class="relative shadow-lg rounded-full  h-40 w-40 sm:h-40 sm:w-40 flex items-center grid justify-items-center  ">
+          <div>
+            <img src={rust} />
+          </div>
+        </div>
+ 
+ 
       </div>
     </section>
   );
