b308cc82179ae4eda245f082c9f7cba69acd44f4
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-09-17 12:31:48 +0300

    "Update Hero section text and Tailwind config"

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         "slate-gray": "#6D6D6D",
         "pale-blue": "#F5F6FF",
         "white-400": "rgba(255, 255, 255, 0.80)",
-        "wood": "green",
+        "wood": "rgba(255, 255, 255, 0.6)",
       },
       boxShadow: {
         '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
