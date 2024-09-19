3f365c3e3803529e5fdbff52c0da9dd1e9c4af82
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-09-20 01:24:14 +0300

    Removed App.css and react.svg, simplified index.css, and updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {  
+      backgroundImage: {
+        'gradient-head': 'linear-gradient(to bottom right, #006571, #00C0D6)',
+        'gradient-head2':'linear-gradient(to bottom right, #006571, #00A5B8)',
+      },
+      colors:{
+        "darkblue":"#002529",
+        "middleblue":"#005C66",
+        "lightblue":"#00A5B8"
+      }
+    },
   },
   plugins: [],
 }
